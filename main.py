79fadc5e6b3225f4d196fa9214ac0c5c85cb3725
Reda2200 <matteoredx@gmail.com>
2023-11-15 12:49:06 +0100

    New script

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 #!/usr/bin/env python3
-
+import os
+# Implementare il main menu in modalità user friendly
 def main():
-    print('Che operazione vuoi fare?\n1. Somma\n2. Sottrazione\n3. Moltiplicazione\n4. Divisione\ne. Esci')
     while True:
+        #os.system('cls' if os.name == 'nt' else 'clear')
+        print('\nChe operazione vuoi fare?\n1. Somma\n2. Sottrazione\n3. Moltiplicazione\n4. Divisione\ne. Esci')
         job = input('Selezione: ')
         if job in ('1', '2', '3', '4'):
-            num1 = float(input('inserisci il primo numero: '))
-            num2 = float(input('Inserisci il secondo numero: '))
+            while True:
+                try:
+                    num1 = float(input("Inserisci il primo numero: "))
+                    break
+                except ValueError:
+                    print("Il numero inserito non è valido")
+                    os.system('cls' if os.name == 'nt' else 'clear')
+            while True:
+                try:
+                    num2 = float(input("Inserisci il secondo numero: "))
+                    break
+                except ValueError:
+                    print("Il numero inserito non è valido")
+
             if job == '1':
-                print(num1+num2)
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print('Risultato: ', num1+num2)
             if job == '2':
-                print(num1-num2)
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print('Risultato: ',num1-num2)
             if job == '3':
-                print(num1*num2)
+                os.system('cls' if os.name == 'nt' else 'clear')
+                print('Risultato: ',num1*num2)
             if job == '4':
-                print(num1/num2)
+                os.system('cls' if os.name == 'nt' else 'clear')
+                try:
+                    print('Risultato: ',num1/num2)
+                except:
+                    print('operazione non valida')
         elif job == 'e':
             print('Uscita')
             break
