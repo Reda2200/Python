e7331799445c71d4b5f7c3e42fd89edadc2a657d
Reda2200 <matteoredx@gmail.com>
2023-11-15 10:54:20 +0100

    removing files old files

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
-#!/usr/bin/env python3
-
 def main():
-    print()
-    name = input('Come ti chiami?\n')
-    if name.lower() == 'matteo':
-        print('Ciao Capo')
-    else:
-        print('Vai via')
-
-
+    print('Che operazione vuoi fare?\n1. Somma\n2. Sottrazione\n3. Moltiplicazione\n4. Divisione\ne. Esci')
+    while True:
+        job = input('Selezione: ')
+        if job in ('1', '2', '3', '4'):
+            num1 = float(input('inserisci il primo numero: '))
+            num2 = float(input('Inserisci il secondo numero: '))
+            if job == '1':
+                print(num1+num2)
+            if job == '2':
+                print(num1-num2)
+            if job == '3':
+                print(num1*num2)
+            if job == '4':
+                print(num1/num2)
+        elif job == 'e':
+            print('Uscita')
+            break
+        else:
+            print('Selezione non valida')
 # execute
 if __name__ == '__main__':
     main()
